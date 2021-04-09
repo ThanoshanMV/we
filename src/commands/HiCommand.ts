53eda6fd6620022ec9795eddfdbc5f5737293674
MV Thanoshan <mvthanoshan9@gmail.com>
2021-04-09 15:04:15 +0530

    refactor code

--- src/commands/HiCommand.ts
+++ src/commands/HiCommand.ts
@@ -1,13 +1,3 @@
-// export class HiCommand {
-//     name: string;
-//     constructor(name: string){
-//         this.name = name;
-//     }
-//     run(): void{
-//         console.log(`Welcome to we CLI ${this.name}`);
-//     }
-// }
-
 import {Command} from './Command';
 
 export class HiCommand implements Command{
--- src/we.ts
+++ src/we.ts
@@ -7,6 +7,10 @@ const args = process.argv;
 const commands = ["hi", "help", "status"];
 const userArgs = args.slice(2);
 
+// log all CLI arguments
+console.log(args);
+
+// log user entered arguments
 console.log(userArgs);
 
 // the help guide
@@ -25,20 +29,20 @@ const helpGuide = function () {
   console.log(helpText);
 };
 
-// log errors to the console in red color
+// log errors to the console in red color, if any
 function errorLog(error: string) {
   const eLog = chalk.red(error);
   console.log(eLog);
 }
 
-// if user entered CLI args are zero or args isn't valid one, prompt help guide and an error message
+// if user doesn't enter any words or words aren't valid ones, prompt help guide and an error message
 if (userArgs.length == 0 || commands.indexOf(userArgs[0]) == -1) {
   errorLog("Please follow the help guide!");
   helpGuide();
 }
 else {
   // valid arg
-  // call the command factory
+  // call the command factory and get appropriate command
   const factoryObject = new CommandFactory();
   const commandObject = factoryObject.getCommand(userArgs[0]);
   commandObject.run();
