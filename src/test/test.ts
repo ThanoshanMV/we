7b0adb355069e42f3f5b3dc98811d40b1360f8ac
MV Thanoshan <mvthanoshan9@gmail.com>
2021-05-14 08:53:55 +0530

    refactor: test grouply feature wise, simplify build cmd

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,31 +1,51 @@
 import {CommandFactory} from '../CommandFactory';
+import { ErrorCommand } from '../commands/ErrorCommand';
+import { HelpCommand } from '../commands/HelpCommand';
 import { HiCommand } from '../commands/HiCommand';
+import { StatusCommand } from '../commands/StatusCommand';
 
 const assert = require('assert');
 // describe: logical grouping of tests
-describe('Test command contents', () => {
-    it('Hi command\'s content', () => {
+describe('Test Error Command', () => {
+    it('Error command\'s content', () => {
         // assert.equal(actual, expected);
-        assert.equal(new CommandFactory().getCommandContent("hi"), "Welcome to we CLI!");
+        assert.equal((new CommandFactory().getCommandContent("randomStuff")), "Sorry! no keywords found. Please type 'Help' see what I can do for you :)");
     });
 
-    it('error command\'s content', () => {
-        assert.equal(new CommandFactory().getCommandContent("i"), "Sorry! no keywords found. Please type 'Help' see what I can do for you :)");
-    });
-
-    it('status command\'s content', () => {
-        assert.equal(new CommandFactory().getCommandContent("status"), "Checkout the status");
+    it('Error command\'s type', () => {
+        assert.equal((new CommandFactory().getCommand("randomStuff") instanceof ErrorCommand), true);
     });
 });
 
-describe('Test command instance type', () => {
-    it('Hi command\'s content', () => {
-        // assert.equal(actual, expected);
-        assert.equal((new CommandFactory().getCommand("hi") instanceof HiCommand), true);
-    });
-
+describe('Test Help Command', () => {
     it('Help command\'s content', () => {
         // assert.equal(actual, expected);
-        assert.equal((new CommandFactory().getCommand("help") instanceof HiCommand), false);
+        assert.equal((new CommandFactory().getCommandContent("help")), "we is a friendly CLI! usage: we <command> commands can be: hi: used to welcome to the userhelp: used to print the usage guide");
+    });
+
+    it('Help command\'s type', () => {
+        assert.equal((new CommandFactory().getCommand("help") instanceof HelpCommand), true);
+    });
+});
+
+describe('Test Hi Command', () => {
+    it('Hi command\'s content', () => {
+        // assert.equal(actual, expected);
+        assert.equal((new CommandFactory().getCommandContent("hi")), "Welcome to we CLI!");
+    });
+
+    it('Hi command\'s type', () => {
+        assert.equal((new CommandFactory().getCommand("hi") instanceof HiCommand), true);
+    });
+});
+
+describe('Test Status Command', () => {
+    it('Status command\'s content', () => {
+        // assert.equal(actual, expected);
+        assert.equal((new CommandFactory().getCommandContent("status")), "Checkout the status");
+    });
+
+    it('Status command\'s type', () => {
+        assert.equal((new CommandFactory().getCommand("status") instanceof StatusCommand), true);
     });
 });
